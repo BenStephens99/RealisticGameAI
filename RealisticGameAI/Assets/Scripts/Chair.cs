a8442d4fef0089a6c70214c9e63fedef88b4e928
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2022-03-15 15:07:52 +0000

    .5

---
+++ RealisticGameAI/Assets/Scripts/Chair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : MonoBehaviour
+{
+    public bool occupied;
+    public NPC npc;
+
+    public void add(NPC n) {
+        npc = n;
+        occupied = true;
+    }
+
+    public void remove(NPC n) {
+        npc = null;
+        occupied = false;
+    }
+}
---
+++ RealisticGameAI/Assets/Scripts/Table.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    public List<NPC> npcs;
+    public Chair[] chairs;
+    public int max;
+    public int current;
+
+    void Awake () {
+        chairs = GetComponentsInChildren<Chair>();
+        max = chairs.Length;
+    }
+    public void add(NPC npc) {
+        if (current < max) {
+            npcs.Add(npc);
+            current++;
+        } else {
+            Debug.Log("Table Full");
+        }
+    }
+
+    public void remove(NPC npc){
+        npcs.Remove(npc);
+        current--;
+    }
+
+}
